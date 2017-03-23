626611d06cc38cde6b7e94fafaf55cbbb44eed62
vigorjao <vigor.jao@eitc.com.tw>
2017-03-23 23:31:52 +0800

    http setting
    使angular 可以跨網域連線api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 // import {RouterModule} from '@angular/router'
 
-
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
